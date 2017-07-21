fd0695d0afd48297594540111e6833c06c5af8aa
Kool Havis <havisk@gmail.com>
2017-07-20 23:33:35 -0400

    add coundownform component and tests

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,19 +1,17 @@
 let React = require('react');
 let Nav = require('Nav');
 
-
-
 let Main = (props) => {
-    return(
+    return (
         <div>
-            <div>
-                <div>
-                    <Nav/>
+            <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
             </div>
         </div>
-    )
-};
+    );
+}
 
 module.exports = Main;
